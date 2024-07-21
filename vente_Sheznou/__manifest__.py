31a63bc0e3dadde850462f8e2ca8eebcbb151cc4
aliadilanielfayaz <aliadilanielfayaz@yahoo.fr>
2024-07-21 20:31:08 +0000

    odification de la version du modul vente_volkeno sur le manifest

--- vente_Sheznou/__manifest__.py
+++ vente_Sheznou/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": " VENTES: ",
     'summary': """Gestion des ventes de Sheznou""",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author":"Mouhamadou Moustapha DIALLO, Mouhamadou Hafiz KA et Ali Adilani EL FAYAZ ",
     "website" : "",
     "category" : "", 
